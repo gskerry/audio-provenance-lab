8431534b1589ff8809e5d7e4b23db497ae5295f6
gskerry <gskerry@users.noreply.github.com>
2024-11-27 18:09:29 -0500

    scripting, add WM

--- audiowmark/awm_add.sh
+++ audiowmark/awm_add.sh
@@ -4,3 +4,14 @@ STRING='Artist:ArtistName|Title:SongTitle|file:artist_song.wav|For:Recipient'
 # echo "$STRING"
 MSG=`echo -n "$STRING" | sha256sum | head -c 32`
 # echo $MSG
+
+docker run \
+-v $DockerHostShareDir:/data \
+--rm \
+-i audiowmark \
+add \
+--strength 11 \
+--key 2024-ILI.key \
+song.wav \
+song_.wav \
+$MSG
